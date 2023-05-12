e45b304dc89e9648fee1070612523bbbb59be9c8
RomanVakulenko <vakula_48@mail.ru>
2023-05-12 14:06:30 +0300

    2nd branch: added ProfileHeaderView.swift, ProfileViewController.swift (it's color changed to .lightGray) into Profile group

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Roman Vakulenko on 12.05.2023.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
