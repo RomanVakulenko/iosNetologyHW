931b4f1f00be4eed7fa01e4b14a5184fb490d7c3
RomanVakulenko <vakula_48@mail.ru>
2023-05-18 09:44:29 +0300

    Dislike the positioning textField and button. Seems like safe area height is ingnoring - what is wrong?

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -8,8 +8,43 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
-//Перейдите в файл ProfileHeaderView и, следуя макету, добавьте необходимые UI-элементы в качестве подпредставлений и установите для них нужные параметры. Ваш экран Profile должен в точности повторять макет. Уделите особое внимание отступам и шрифтам. Имя, подпись и аватарку можно выбрать по своему усмотрению :)
 
+    let avatarView: UIView
+    let labelView: UILabel
 
+    override init(frame: CGRect) {
+        avatarView = UIView()
+        labelView = UILabel()
+        super.init(frame: frame)
+        addSubview(avatarView)
+        addSubview(labelView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setAvatar()
+        setLabel()
+    }
+
+    private func setAvatar() {
+        let imageView = UIImageView(image: UIImage(named: "Colors"))
+        imageView.frame = CGRect(x: 16, y: 16, width: 100, height: 100)
+        imageView.layer.cornerRadius = imageView.frame.width/2
+        imageView.clipsToBounds = true
+        (imageView.layer.borderWidth, imageView.layer.borderColor) = (3, UIColor.white.cgColor)
+        avatarView.addSubview(imageView)
+    }
+
+    private func setLabel() {
+        let label = UILabel(frame: CGRect(x: self.center.x-50, y: 27, width: 200, height: 20))
+        label.font = UIFont.boldSystemFont(ofSize: 18.0)
+        label.textColor = .black
+        label.text = "Colors in iOS"
+        labelView.addSubview(label)
+    }
 
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,19 +9,71 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-//Создайте экземпляр класса ProfileHeaderView в классе ProfileViewController, добавьте его в качестве subview и в методе viewWillLayoutSubviews() задайте ему frame, равный frame корневого view.
     let profileHeaderView = ProfileHeaderView()
+    var textField: UITextField?
+
+    required convenience init?(coder aDecoder: NSCoder, textField: UITextField) {
+        self.init(coder: aDecoder)
+        self.textField = textField
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
-
         view.addSubview(profileHeaderView)
     }
 
     override func viewWillLayoutSubviews() {
-        profileHeaderView.frame = self.view.frame
+        super.viewDidLayoutSubviews()
+        profileHeaderView.frame = self.view.safeAreaLayoutGuide.layoutFrame//к моменту отрисовки subview размеры superView будут корректны (потому тутб а не в ДидЛоад)
+        setTextField()
+        setButton()
     }
 
+    func setTextField() {
+        textField = UITextField(frame: CGRect(x: 157, y: (2*16+200)-34-50, width: 200, height: 50))
+        textField?.placeholder = "Waiting for something..."
+        textField?.font = UIFont.systemFont(ofSize: 14)
+        textField?.textColor = .gray
+        self.view.addSubview(textField!)
+//        textField.addTarget(self, action: #selector(textFieldEditingDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+
+    func setButton(){
+        let showStatusButton = UIButton(frame: CGRect(x: 16, y: (2*16+200), width: self.view.frame.width-32, height: 50))
+        showStatusButton.backgroundColor = .systemBlue
+        showStatusButton.layer.cornerRadius = 4
+        showStatusButton.setTitle("Show status", for: .normal)
+        showStatusButton.setTitleColor(UIColor.white, for: .normal)
+        (showStatusButton.layer.shadowOffset.width, showStatusButton.layer.shadowOffset.height, showStatusButton.layer.shadowRadius) = (4, 4, 4)
+        showStatusButton.layer.shadowColor = UIColor.black.cgColor
+        showStatusButton.layer.shadowOpacity = 0.7
+
+        showStatusButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        self.view.addSubview(showStatusButton)
+    }
+
+    @objc func buttonPressed(_ sender: UIButton) {
+        guard let status = textField?.text else {return}
+        print(status)
+    }
+//    @objc func textFieldEditingDidChange (_ sender: Any) {
+//        print("textField: \(String(describing: textField.text))")
+//
+//        if didTyped(text: textField.text) {
+//                // correct password
+//                button.isEnabled = true
+//            } else {
+//                button.isEnabled = false
+//            }
+//    }
+//
+//    func didTyped(text: String?) -> Bool {
+//        var result = false
+//        if text != nil {
+//            result = true
+//        }
+//        return result
+//    }
 
 }
