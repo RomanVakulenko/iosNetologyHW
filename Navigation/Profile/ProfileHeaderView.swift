caa1f5ee4d5a036c5d0f8163c91b8221a3a34bd3
RomanVakulenko <vakula_48@mail.ru>
2023-05-18 22:11:09 +0300

    created button at ProfileViewController. Text in textField typing, but tap at button doesn't change status

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -13,7 +13,6 @@ class ProfileHeaderView: UIView {
     let nameLabel: UILabel?
     let statusLabel: UILabel?
     var textField: UITextField?
-    let button: UIButton?
 
     var statusText: String?
 
@@ -22,7 +21,6 @@ class ProfileHeaderView: UIView {
         nameLabel = UILabel()
         statusLabel = UILabel()
         textField = UITextField()
-        button = UIButton()
         super.init(frame: frame)
     }
 
@@ -36,7 +34,6 @@ class ProfileHeaderView: UIView {
         setNameLabel()
         setStatusLabel()
         setStatusChangingTextField()
-        showStatusButton()
     }
 
     let imageView = UIImageView(image: UIImage(named: "Colors"))
@@ -81,24 +78,5 @@ class ProfileHeaderView: UIView {
     @objc func statusTextChanged(_ sender: UITextField) {
         guard let newStatusText = statusChangingTextField.text else { return }
         statusText = newStatusText
-    }
-
-    func showStatusButton() {
-        let button = UIButton(frame: CGRect(x: 16, y: 2*16+100, width: self.frame.width-32, height: 50))
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 4
-        button.setTitle("Set status", for: .normal)
-        button.setTitleColor(UIColor.white, for: .normal)
-        (button.layer.shadowOffset.width, button.layer.shadowOffset.height, button.layer.shadowRadius) = (4, 4, 4)
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOpacity = 0.7
-        addSubview(button)
-
-        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
-
-    }
-
-    @objc func buttonPressed(_ sender: UIButton) {
-        statusLabel?.text = statusText
     }
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,6 +11,19 @@ class ProfileViewController: UIViewController {
 
     let profileHeaderView = ProfileHeaderView()
 
+    private lazy var button: UIButton = {
+        button = UIButton(frame: CGRect(x: 16, y: profileHeaderView.frame.origin.y+2*16+100, width: profileHeaderView.frame.width-32, height: 50))
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 4
+        button.setTitle("Set status", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        (button.layer.shadowOffset.width, button.layer.shadowOffset.height, button.layer.shadowRadius) = (4, 4, 4)
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+
+        return button
+    }()
+
     required convenience init?(coder aDecoder: NSCoder) {
         self.init(coder: aDecoder)
     }
@@ -24,6 +37,11 @@ class ProfileViewController: UIViewController {
     override func viewWillLayoutSubviews() {
         super.viewDidLayoutSubviews()
         profileHeaderView.frame = self.view.safeAreaLayoutGuide.layoutFrame//к моменту отрисовки subview размеры superView будут корректны (потому тут, а не в ДидЛоад)
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
     }
-    
+
+    @objc func buttonPressed(_ sender: UIButton) {
+        profileHeaderView.statusLabel?.text = profileHeaderView.statusText
+    }
 }
