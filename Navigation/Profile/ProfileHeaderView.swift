07465ccd2d5e80d857e812763d6aa2d8fc0a434b
RomanVakulenko <vakula_48@mail.ru>
2023-05-18 13:57:57 +0300

    1. tap at UITextFiled does nothing (no simulator's keyboard poping) 2. after tap start type using physical keyboard - error (несмотря на то, что обе клавы в симулятор/ i/o / keyboard отмечены/стоят): 2023-05-18 13:51:45.891983+0300 Navigation[7176:212623] [HardwareKeyboard] -[UIApplication getKeyboardDevicePropertiesForSenderID:shouldUpdate:usingSyntheticEvent:], failed to fetch device property for senderID (778835616971358211) use primary keyboard info instead. 3. so...there is no status changing((

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -10,14 +10,26 @@ import UIKit
 class ProfileHeaderView: UIView {
 
     let avatarView: UIView
-    let labelView: UILabel
+    let nameLabel: UILabel
+    let statusLabel: UILabel
+    var textField: UITextField
+    let button: UIButton
+
+    var statusText: String?
 
     override init(frame: CGRect) {
         avatarView = UIView()
-        labelView = UILabel()
+        nameLabel = UILabel()
+        statusLabel = UILabel()
+        textField = UITextField()
+        button = UIButton()
+
         super.init(frame: frame)
         addSubview(avatarView)
-        addSubview(labelView)
+        addSubview(nameLabel)
+        addSubview(statusLabel)
+        addSubview(textField)
+        addSubview(button)
     }
 
     required init?(coder: NSCoder) {
@@ -27,11 +39,13 @@ class ProfileHeaderView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         setAvatar()
-        setLabel()
+        setNameLabel()
+        setStatusLabel()
+        setStatusChangingTextField()
     }
 
+    let imageView = UIImageView(image: UIImage(named: "Colors"))
     private func setAvatar() {
-        let imageView = UIImageView(image: UIImage(named: "Colors"))
         imageView.frame = CGRect(x: 16, y: 16, width: 100, height: 100)
         imageView.layer.cornerRadius = imageView.frame.width/2
         imageView.clipsToBounds = true
@@ -39,12 +53,57 @@ class ProfileHeaderView: UIView {
         avatarView.addSubview(imageView)
     }
 
-    private func setLabel() {
-        let label = UILabel(frame: CGRect(x: self.center.x-50, y: 27, width: 200, height: 20))
+    let label = UILabel()
+    private func setNameLabel() {
+        label.frame = CGRect(x: self.center.x-50, y: 18, width: 200, height: 20)//когда 27 top - некрасиво
         label.font = UIFont.boldSystemFont(ofSize: 18.0)
         label.textColor = .black
         label.text = "Colors in iOS"
-        labelView.addSubview(label)
+        nameLabel.addSubview(label)
+    }
+
+    private func setStatusLabel() {
+        let status = UILabel(frame: CGRect(x: self.center.x-50, y: 16+(100/2)-20, width: 200, height: 20))
+        status.text = "Waiting for something..."
+        status.font = UIFont.systemFont(ofSize: 14)
+        status.textColor = .gray
+        statusLabel.addSubview(status)
+    }
+
+    func setStatusChangingTextField() {
+        let statusChangingTextField = UITextField(frame: CGRect(x: self.center.x-50, y: 16+100-40, width: self.frame.width/2, height: 40))
+        statusChangingTextField.placeholder = "  Type new status"
+        statusChangingTextField.font = UIFont.systemFont(ofSize: 15)
+        statusChangingTextField.textColor = .black
+        statusChangingTextField.backgroundColor = .white
+        (statusChangingTextField.layer.borderWidth, statusChangingTextField.layer.borderColor) = (1, UIColor.black.cgColor)
+        statusChangingTextField.layer.cornerRadius = 12
+        textField.addSubview(statusChangingTextField)
+
+        statusChangingTextField.addTarget(self, action: #selector(statusTextChanged(_:)), for: UIControl.Event.editingChanged)
+    }
+
+    @objc func statusTextChanged(_ sender: UITextField) {
+        guard let newStatusText = textField.text else { return }
+        statusText = newStatusText
+    }
+
+    func setButton(){
+        let showStatusButton = UIButton(frame: CGRect(x: 16, y: 2*16+100, width: self.frame.width-32, height: 50))
+        showStatusButton.backgroundColor = .systemBlue
+        showStatusButton.layer.cornerRadius = 4
+        showStatusButton.setTitle("Set status", for: .normal)
+        showStatusButton.setTitleColor(UIColor.white, for: .normal)
+        (showStatusButton.layer.shadowOffset.width, showStatusButton.layer.shadowOffset.height, showStatusButton.layer.shadowRadius) = (4, 4, 4)
+        showStatusButton.layer.shadowColor = UIColor.black.cgColor
+        showStatusButton.layer.shadowOpacity = 0.7
+        button.addSubview(showStatusButton)
+
+        showStatusButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+    }
+
+    @objc func buttonPressed(_ sender: UIButton) {
+        statusLabel.text = statusText
     }
 
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -10,11 +10,9 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     let profileHeaderView = ProfileHeaderView()
-    var textField: UITextField?
 
-    required convenience init?(coder aDecoder: NSCoder, textField: UITextField) {
+    required convenience init?(coder aDecoder: NSCoder) {
         self.init(coder: aDecoder)
-        self.textField = textField
     }
 
     override func viewDidLoad() {
@@ -26,54 +24,9 @@ class ProfileViewController: UIViewController {
     override func viewWillLayoutSubviews() {
         super.viewDidLayoutSubviews()
         profileHeaderView.frame = self.view.safeAreaLayoutGuide.layoutFrame//к моменту отрисовки subview размеры superView будут корректны (потому тутб а не в ДидЛоад)
-        setTextField()
-        setButton()
+        profileHeaderView.setButton()
     }
 
-    func setTextField() {
-        textField = UITextField(frame: CGRect(x: 157, y: profileHeaderView.frame.origin.y+(2*16+100)-34-50, width: 200, height: 50))
-        textField?.placeholder = "Waiting for something..."
-        textField?.font = UIFont.systemFont(ofSize: 14)
-        textField?.textColor = .gray
-        self.view.addSubview(textField!)
-//        textField.addTarget(self, action: #selector(textFieldEditingDidChange(_:)), for: UIControl.Event.editingChanged)
-    }
 
-    func setButton(){
-        let showStatusButton = UIButton(frame: CGRect(x: 16, y: profileHeaderView.frame.origin.y+(2*16+100), width: self.view.frame.width-32, height: 50))
-        showStatusButton.backgroundColor = .systemBlue
-        showStatusButton.layer.cornerRadius = 4
-        showStatusButton.setTitle("Show status", for: .normal)
-        showStatusButton.setTitleColor(UIColor.white, for: .normal)
-        (showStatusButton.layer.shadowOffset.width, showStatusButton.layer.shadowOffset.height, showStatusButton.layer.shadowRadius) = (4, 4, 4)
-        showStatusButton.layer.shadowColor = UIColor.black.cgColor
-        showStatusButton.layer.shadowOpacity = 0.7
-
-        showStatusButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
-        self.view.addSubview(showStatusButton)
-    }
-
-    @objc func buttonPressed(_ sender: UIButton) {
-        guard let status = textField?.text else {return}
-        print(status)
-    }
-//    @objc func textFieldEditingDidChange (_ sender: Any) {
-//        print("textField: \(String(describing: textField.text))")
-//
-//        if didTyped(text: textField.text) {
-//                // correct password
-//                button.isEnabled = true
-//            } else {
-//                button.isEnabled = false
-//            }
-//    }
-//
-//    func didTyped(text: String?) -> Bool {
-//        var result = false
-//        if text != nil {
-//            result = true
-//        }
-//        return result
-//    }
 
 }
