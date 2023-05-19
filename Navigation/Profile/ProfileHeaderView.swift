f9421b5aa0f41448ce6b52fcaebaf8542103f4e5
RomanVakulenko <vakula_48@mail.ru>
2023-05-19 12:40:35 +0300

    private lazy properties + button.addtarget = success. Only questions about frames and layout still ARE!! Please give answers!

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,18 +9,52 @@ import UIKit
 
 class ProfileHeaderView: UIView {
 
-    let avatarView: UIView?
-    let nameLabel: UILabel?
-    let statusLabel: UILabel?
-    var textField: UITextField?
-
-    var statusText: String?
+    private lazy var avatarView: UIView = {
+        let imageView = UIImageView(image: UIImage(named: "Colors"))
+        imageView.frame = CGRect(x: 16, y: 16, width: 100, height: 100)
+        imageView.layer.cornerRadius = imageView.frame.width/2
+        imageView.clipsToBounds = true
+        (imageView.layer.borderWidth, imageView.layer.borderColor) = (3, UIColor.white.cgColor)
+        return imageView
+    }()
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: self.center.x-50, y: 18, width: 200, height: 20)//когда 27 top - некрасиво
+        label.font = UIFont.boldSystemFont(ofSize: 18.0)
+        label.textColor = .black
+        label.text = "Colors in iOS"
+        return label
+    }()
+    private lazy var statusLabel: UILabel = {
+        let status = UILabel(frame: CGRect(x: 157, y: 16+(100/2)-20, width: 200, height: 20))
+        status.text = "Waiting for something..."
+        status.font = UIFont.systemFont(ofSize: 14)
+        status.textColor = UIColor.gray
+        return status
+    }()
+    private lazy var textField: UITextField = {
+        let statusChangingTextField = UITextField(frame: CGRect(x: 156, y: 16+100-40, width: 200, height: 40))
+        statusChangingTextField.placeholder = "  Type new status"
+        statusChangingTextField.font = UIFont.systemFont(ofSize: 15)
+        statusChangingTextField.textColor = UIColor.black
+        statusChangingTextField.backgroundColor = UIColor.white
+        (statusChangingTextField.layer.borderWidth, statusChangingTextField.layer.borderColor) = (1, UIColor.black.cgColor)
+        statusChangingTextField.layer.cornerRadius = 12
+        return statusChangingTextField
+    }()
+    private lazy var button: UIButton = {
+        button = UIButton(frame: CGRect(x: 16, y: 2*16+100, width: self.frame.width-32, height: 50))
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 4
+        button.setTitle("Set status", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        (button.layer.shadowOffset.width, button.layer.shadowOffset.height, button.layer.shadowRadius) = (4, 4, 4)
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        return button
+    }()
 
     override init(frame: CGRect) {
-        avatarView = UIView()
-        nameLabel = UILabel()
-        statusLabel = UILabel()
-        textField = UITextField()
         super.init(frame: frame)
     }
 
@@ -30,53 +64,16 @@ class ProfileHeaderView: UIView {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        setAvatar()
-        setNameLabel()
-        setStatusLabel()
-        setStatusChangingTextField()
+        addSubview(avatarView)
+        addSubview(nameLabel)
+        addSubview(statusLabel)
+        addSubview(textField)
+        addSubview(button)
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
     }
 
-    let imageView = UIImageView(image: UIImage(named: "Colors"))
-    private func setAvatar() {
-        imageView.frame = CGRect(x: 16, y: 16, width: 100, height: 100)
-        imageView.layer.cornerRadius = imageView.frame.width/2
-        imageView.clipsToBounds = true
-        (imageView.layer.borderWidth, imageView.layer.borderColor) = (3, UIColor.white.cgColor)
-        addSubview(imageView)
+    @objc func buttonPressed(_ sender: UIButton) {
+        statusLabel.text = textField.text
     }
 
-    let label = UILabel()
-    private func setNameLabel() {
-        label.frame = CGRect(x: self.center.x-50, y: 18, width: 200, height: 20)//когда 27 top - некрасиво
-        label.font = UIFont.boldSystemFont(ofSize: 18.0)
-        label.textColor = .black
-        label.text = "Colors in iOS"
-        addSubview(label)
-    }
-
-    private func setStatusLabel() {
-        let status = UILabel(frame: CGRect(x: self.center.x-50, y: 16+(100/2)-20, width: 200, height: 20))
-        status.text = "Waiting for something..."
-        status.font = UIFont.systemFont(ofSize: 14)
-        status.textColor = .gray
-        addSubview(status)
-    }
-
-    let statusChangingTextField = UITextField(frame: CGRect(x: 157, y: 16+100-40, width: 200, height: 40))
-    func setStatusChangingTextField() {
-        statusChangingTextField.placeholder = "  Type new status"
-        statusChangingTextField.font = UIFont.systemFont(ofSize: 15)
-        statusChangingTextField.textColor = UIColor.black
-        statusChangingTextField.backgroundColor = UIColor.white
-        (statusChangingTextField.layer.borderWidth, statusChangingTextField.layer.borderColor) = (1, UIColor.black.cgColor)
-        statusChangingTextField.layer.cornerRadius = 12
-        addSubview(statusChangingTextField)
-
-        statusChangingTextField.addTarget(self, action: #selector(statusTextChanged(_:)), for: UIControl.Event.editingChanged)
-        }
-
-    @objc func statusTextChanged(_ sender: UITextField) {
-        guard let newStatusText = statusChangingTextField.text else { return }
-        statusText = newStatusText
-    }
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,37 +11,18 @@ class ProfileViewController: UIViewController {
 
     let profileHeaderView = ProfileHeaderView()
 
-    private lazy var button: UIButton = {
-        button = UIButton(frame: CGRect(x: 16, y: profileHeaderView.frame.origin.y+2*16+100, width: profileHeaderView.frame.width-32, height: 50))
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 4
-        button.setTitle("Set status", for: .normal)
-        button.setTitleColor(UIColor.white, for: .normal)
-        (button.layer.shadowOffset.width, button.layer.shadowOffset.height, button.layer.shadowRadius) = (4, 4, 4)
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOpacity = 0.7
-
-        return button
-    }()
-
     required convenience init?(coder aDecoder: NSCoder) {
         self.init(coder: aDecoder)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .lightGray
+        view.backgroundColor = .lightGray
         view.addSubview(profileHeaderView)
     }
 
     override func viewWillLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        profileHeaderView.frame = self.view.safeAreaLayoutGuide.layoutFrame//к моменту отрисовки subview размеры superView будут корректны (потому тут, а не в ДидЛоад)
-        view.addSubview(button)
-        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
-    }
-
-    @objc func buttonPressed(_ sender: UIButton) {
-        profileHeaderView.statusLabel?.text = profileHeaderView.statusText
+        profileHeaderView.frame = view.safeAreaLayoutGuide.layoutFrame//к моменту отрисовки subview размеры superView будут корректны (потому тут, а не в ДидЛоад)
     }
 }
