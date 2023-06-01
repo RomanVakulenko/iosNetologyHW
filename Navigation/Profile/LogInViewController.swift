f33d5cca2182167e99e43b7d680ddf2683f8cdd4
RomanVakulenko <vakula_48@mail.ru>
2023-06-01 11:56:08 +0300

    textFields are in StackView + alignment + underlineBySublayer + button works

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 final class LogInViewController: UIViewController {
 
-    private let loginModel = LogInModel.makeMockModel()
-
     private lazy var logoView: UIView = {
         let image = UIImageView(image: UIImage(named: "vkLogo"))
         image.clipsToBounds = true
@@ -18,34 +16,80 @@ final class LogInViewController: UIViewController {
         return image
     }()
 
-    private lazy var tableView: UITableView = {
-        let table = UITableView()
-        table.translatesAutoresizingMaskIntoConstraints = false
-        table.dataSource = self
-        table.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.cellIdentifier)
-        return table
+    private let textAlignment: CGFloat = 12
+    private lazy var logInTextFieldView: UITextField = {
+        let logInText = UITextField()
+        logInText.translatesAutoresizingMaskIntoConstraints = false
+        logInText.placeholder = "  Email or phone"
+        logInText.layer.sublayerTransform = CATransform3DMakeTranslation(textAlignment, 0, 0)
+        logInText.font = .systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+        logInText.tintColor = .lightGray
+        logInText.autocapitalizationType = .none
+        return logInText
     }()
 
-//    private lazy var buttonView: UIButton = {
-//        let logInButton = UIButton()
-//        logInButton.backgroundColor = UIColor(hex: 0x4885CC)
-//        logInButton.layer.cornerRadius = 10
-//        logInButton.titleLabel?.text = "Log in"
-//        return logInButton
-//    }()
+    private lazy var passwordTextFieldView: UITextField = {
+        let passwordText = UITextField()
+        passwordText.translatesAutoresizingMaskIntoConstraints = false
+        passwordText.placeholder = "  Password"
+        passwordText.layer.sublayerTransform = CATransform3DMakeTranslation(textAlignment, 0, 0)
+        passwordText.textColor = .black
+        passwordText.font = .systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+        passwordText.tintColor = .lightGray
+        passwordText.autocapitalizationType = .none
+        return passwordText
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.clipsToBounds = true
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.spacing = 0
+        [logInTextFieldView, passwordTextFieldView].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
+
+    private lazy var buttonView: UIButton = {
+        let logInButton = UIButton()
+        logInButton.translatesAutoresizingMaskIntoConstraints = false
+        logInButton.backgroundColor = UIColor(hex: 0x4885CC)
+        logInButton.layer.cornerRadius = 10
+        logInButton.setTitle("Log in", for: .normal)
+        logInButton.addTarget(self, action: #selector(logInPressed(_:)), for: .touchUpInside)
+        return logInButton
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-//        [logoView, tableView].forEach { view.addSubview($0) }
-        view.addSubview(tableView)
-        view.addSubview(logoView)
-//        view.addSubview(buttonView)
+        [logoView, stackView, buttonView].forEach { view.addSubview($0) }
         layout()
+        customizeStackView()
     }
 
+    override func viewWillLayoutSubviews() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: -textAlignment, y: logInTextFieldView.frame.height-1.0, width: logInTextFieldView.frame.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor.lightGray.cgColor
+        logInTextFieldView.borderStyle = .none
+        logInTextFieldView.layer.addSublayer(bottomLine)
+    }
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = true
+    }
+
+    private func customizeStackView(){
+        stackView.layer.borderColor = UIColor.lightGray.cgColor
+        stackView.layer.borderWidth = 0.5
+        stackView.layer.cornerRadius = 10
+        stackView.backgroundColor = .systemGray6
+    }
+
+    @objc func logInPressed(_ sender: UIButton){
+        let profileViewController = ProfileViewController()
+        self.navigationController?.pushViewController(profileViewController, animated: true)
     }
 
     private func layout(){
@@ -56,34 +100,29 @@ final class LogInViewController: UIViewController {
             logoView.widthAnchor.constraint(equalToConstant: 100),
             logoView.heightAnchor.constraint(equalToConstant: 100),
 
-            tableView.centerXAnchor.constraint(equalTo: safearea.centerXAnchor),
-            tableView.topAnchor.constraint(equalTo: logoView.bottomAnchor, constant: 120),
-            tableView.leadingAnchor.constraint(equalTo: safearea.leadingAnchor, constant: 16),
-            tableView.trailingAnchor.constraint(equalTo: safearea.trailingAnchor, constant: -16),
-            tableView.heightAnchor.constraint(equalToConstant: 100),
+            stackView.centerXAnchor.constraint(equalTo: safearea.centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: logoView.bottomAnchor, constant: 120),
+            stackView.leadingAnchor.constraint(equalTo: safearea.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: safearea.trailingAnchor, constant: -16),
+            stackView.heightAnchor.constraint(equalToConstant: 100),
 
-//            buttonView.centerXAnchor.constraint(equalTo: safearea.centerXAnchor),
-//            buttonView.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 16),
-//            buttonView.leadingAnchor.constraint(equalTo: safearea.leadingAnchor, constant: 16),
-//            buttonView.trailingAnchor.constraint(equalTo: safearea.trailingAnchor, constant: -16),
-//            buttonView.heightAnchor.constraint(equalToConstant: 50)
+            logInTextFieldView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            logInTextFieldView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            logInTextFieldView.topAnchor.constraint(equalTo: stackView.topAnchor),
+            logInTextFieldView.heightAnchor.constraint(equalToConstant: 50),
+
+            passwordTextFieldView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            passwordTextFieldView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            passwordTextFieldView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+            passwordTextFieldView.heightAnchor.constraint(equalToConstant: 50),
+
+            buttonView.centerXAnchor.constraint(equalTo: safearea.centerXAnchor),
+            buttonView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 16),
+            buttonView.leadingAnchor.constraint(equalTo: safearea.leadingAnchor, constant: 16),
+            buttonView.trailingAnchor.constraint(equalTo: safearea.trailingAnchor, constant: -16),
+            buttonView.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 
 }
 
-extension LogInViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        loginModel.count
-        1
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.cellIdentifier, for: indexPath) as? CustomTableViewCell else {
-            fatalError("can not dequeue ReusableCell")
-        }
-//        cell.
-        return cell
-//        UITableViewCell()
-    }
-}
